f836882e724d9ba59a0cf97e89b00bcd6f8ee94e
Aradhika Acharya <aradhika.acharya@gmail.com>
2017-07-13 22:27:36 +0530

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,3 +1,12 @@
 f = open("/home/aradhika/Downloads/movie_metadata.csv", "r")
-data = f.read()
+movies = f.read()
+
+split_movies = movies.split("\n")
+print(split_movies[0:5])
+movie_data = []
+for each in split_movies:
+    movie_data.append(each.split(","))
+
+print(movie_data[0:5])
+
 
